f89173c67a9310849993e7b7ba5a4b900485a7cf
xfroggy <tc.smitala@gmail.com>
2021-05-05 09:48:46 -0400

    create show route and MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_151323) do
+ActiveRecord::Schema.define(version: 2021_05_04_152235) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
