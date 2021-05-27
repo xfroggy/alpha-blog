e5cd6df444453495d80cb639cf4ea5c5e99ed050
xfroggy <tc.smitala@gmail.com>
2021-05-27 18:47:33 -0400

    Add password hash column to db with BCrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_132330) do
+ActiveRecord::Schema.define(version: 2021_05_27_223836) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_132330) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
